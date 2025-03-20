748dca58b6d83d4b574cd8b5e8f53db9697be104
Jay Tan <jay@keq.com.au>
2025-03-20 11:37:42 +1100

    update cors header to allow access from frontend

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from typing import Annotated
 from fastapi import FastAPI, Depends, HTTPException, Query
+from fastapi.middleware.cors import CORSMiddleware
 from .models import WeatherStationPublic, WeatherStation
 from .database import get_session, engine
 
 from sqlmodel import SQLModel, create_engine, Session, select
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 SessionDep = Annotated[Session, Depends(get_session)]        
 
